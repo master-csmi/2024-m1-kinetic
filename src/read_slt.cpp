df553da3ea67d00db0ba3f97e8c99ffdd9df6b0d
infmathAxel <axeldemuth67150@gmail.com>
2024-05-20 16:20:21 +0200

    convert MSH and STL to supported CGAL format file #32 read stl

--- src/read_slt.cpp
+++ src/read_slt.cpp
@@ -1,22 +1,25 @@
 #include <CGAL/Simple_cartesian.h>
-#include <CGAL/Polyhedron_3.h>
-#include <CGAL/Polyhedron_incremental_builder_3.h>
-#include <CGAL/IO/Polyhedron_iostream.h>
-#include <IO/STL/STL_reader.h>
+#include "../IO/STL/STL_reader.h"
 #include <fstream>
 #include <vector>
 
 typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_3 Point;
-typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <file.stl>" << std::endl;
-        return EXIT_FAILURE;
-    }
 
-    const char* filename = argv[1];
+    const char* filename;
+
+    if (argc != 2) {
+        filename = "../Kinetic_exemple/ACJasmin/mesh.stl";
+    }
+    
+    else {
+    filename = argv[1];
+    }
+    
+    
     std::ifstream input(filename, std::ios::binary);
     if (!input) {
         std::cerr << "Error: cannot open file " << filename << std::endl;
@@ -34,13 +37,6 @@ int main(int argc, char* argv[]) {
     std::cout << "Successfully read STL file: " << filename << std::endl;
     std::cout << "Number of points: " << points.size() << std::endl;
     std::cout << "Number of facets: " << facets.size() << std::endl;
-
-    // Optionally, build a polyhedron
-    Polyhedron P;
-    CGAL::build_from_polygon_soup(points, facets, P);
-
-    std::cout << "Polyhedron built with " << P.size_of_vertices() << " vertices, "
-              << P.size_of_facets() << " facets." << std::endl;
 
     return EXIT_SUCCESS;
 }
